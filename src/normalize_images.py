40e0e9c3c2b4b80dcd376f4ce4e1e511ae7529f6
ShaikFarhana8 <shaikfarhana891@gmail.com>
2026-06-24 17:53:07 +0530

    Add pixel normalization preprocessing

---
+++ src/normalize_images.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+from PIL import Image
+
+dataset_path = "plantvillage dataset/color"
+
+first_class = os.listdir(dataset_path)[0]
+
+class_path = os.path.join(dataset_path, first_class)
+
+first_image = os.listdir(class_path)[0]
+
+image_path = os.path.join(class_path, first_image)
+
+img = Image.open(image_path)
+
+img = img.resize((224, 224))
+
+img_array = np.array(img)
+
+print("Before Normalization")
+print("Min Pixel:", img_array.min())
+print("Max Pixel:", img_array.max())
+
+normalized_img = img_array / 255.0
+
+print("\nAfter Normalization")
+print("Min Pixel:", normalized_img.min())
+print("Max Pixel:", normalized_img.max())
